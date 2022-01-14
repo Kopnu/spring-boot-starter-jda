5adfea4dcb8c3e6510d24ee450483ed61b746ca4
Kopnu <Dzivv0@yandex.ru>
2022-01-14 14:45:14 +0400

    Add LICENSE to project

--- src/main/java/love/korni/jda/spring/config/JdaAutoConfig.java
+++ src/main/java/love/korni/jda/spring/config/JdaAutoConfig.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2022 Sergei Kornilov
+ * Licensed under the Apache License, Version 2.0
+ */
+
 package love.korni.jda.spring.config;
 
 import love.korni.jda.spring.JdaProperties;
